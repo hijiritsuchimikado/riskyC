75b50792919a8f590ae0d02a21a620fbd175a061
hijiritsuchimikado <lemegesor@zohomail.com>
2025-07-15 09:16:44 +0000

    Add files via upload

---
+++ core/stack_mem.h
@@ -0,0 +1,14 @@
+#ifndef _STACK_MEM_H
+#   define _STACK_MEM_H
+
+#include <sys/resource.h>
+
+// return == 0 <=> succeed in changing available stack memory size
+#define stack_mem(mb) ({                    \
+    struct rlimit rl;                       \
+    getrlimit(RLIMIT_STACK, &rl);           \
+    rl.rlim_cur = (unsigned long) mb << 20; \
+    setrlimit(RLIMIT_STACK, &rl);           \
+})
+
+#endif
