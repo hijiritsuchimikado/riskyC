bfc039cf002621d52678710047bd2b74f736c02a
Lemegesor (聖土御門) <lemegesor@zohomail.com>
2025-07-17 22:30:00 +0700

    Update base.h

--- struct/base.h
+++ struct/base.h
@@ -8,7 +8,7 @@ typedef struct {void *str, *end, *max, *ptr;} pd4;
 
 #define dc(x) sml(x.str, x.end)
 
-#define s0i(x, type, num) x.end = x.str = __builtin_alloca(sz(num, type));
+#define s0i(x, type, num) x.end = x.str = __builtin_alloca(sz(num, type))
 // WARNING: You shouldn't change the value of num here: ++, +=, ...
 // ______________________________________________________________________________________________
 #define smi(x, type, num) x.end = x.str = (type*) __builtin_alloca(sz(num, type)) + ((num) >> 1)
