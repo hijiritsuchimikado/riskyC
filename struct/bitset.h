1688365b8316af95e1e572966c5ac80e475d1494
Lemegesor (聖土御門) <lemegesor@zohomail.com>
2025-07-19 21:29:37 +0700

    Update bitset.h

--- struct/bitset.h
+++ struct/bitset.h
@@ -1,18 +1,19 @@
 #ifndef _BITSET_H
 #   define _BITSET_H
 
-#include "../core/base.h"
+#include "base.h"
 
-typedef struct {unsigned long *data;} bitset;
+#define bitset ul1
 
 #define bsp(p) ((p) >> log_lbs)
 #define bsv(x, p) x.data[bsp(p)]
 #define bp(p) (1UL << ((p) & lbsm1))
 #define bss(m) (bsp(m) + 1) << log_lsz
 
-#define bitset_init(x, max)             \
-    x.st = __builtin_alloca(bss(max));  \
-    __builtin_memset(x.st, 0, bss(max))
+#define bitset_init(x, max) \
+    bitset x;               \
+    x.data = al(bss(max));  \
+    ms(x.data, 0, bss(max))
 // bitset_read != 0 <=> bit == 1
 #define bitset_read(x, pos) (bsv(x, pos) & bp(pos))
 #define bitset_write_1(x, pos) bsv(x, pos) |= bp(pos)
