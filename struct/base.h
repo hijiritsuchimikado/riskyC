b6c16d3d285dce18a0f7a21452e306b6921e0d5a
Lemegesor (聖土御門) <lemegesor@zohomail.com>
2025-07-19 21:14:00 +0700

    Update base.h

--- struct/base.h
+++ struct/base.h
@@ -6,32 +6,40 @@
 typedef struct {void *str, *end;} pd2;
 typedef struct {void *str, *end, *max, *ptr;} pd4;
 
+#define al(x) __builtin_alloca(x)
+#define al1(type) al(sizeof(type))
+#define aln(num, type) al(sz(num, type))
+// only works with num == 0 || -1
+#define ms(x, num, type) __builtin_memset(x, num, sz(num, type))
+#define mcp(x, y, size) __builtin_memcpy(x, y, size)
+#define mmv(x, y, size) __builtin_memcpy(x, y, size)
+
 #define dc(x) sml(x.str, x.end)
 
-#define s0i(x, type, num) x.end = x.str = __builtin_alloca(sz(num, type))
+#define s0i(x, type, num) x.end = x.str = aln(num, type)
 // WARNING: You shouldn't change the value of num here: ++, +=, ...
-// ______________________________________________________________________________________________
-#define smi(x, type, num) x.end = x.str = (type*) __builtin_alloca(sz(num, type)) + ((num) >> 1)
+// _____________________________________________________________________________
+#define smi(x, type, num) x.end = x.str = (type*) aln(num, type) + ((num) >> 1)
 #define s04(x, type, num)                           \
-    x.ptr = __builtin_alloca(sz(num, type) + 1);    \
+    x.ptr = al(sz(num, type) + 1);                  \
     x.max = (type*) (x.str = x.end = x.ptr) + (num)
 #define sm4(x, type, num)                           \
-    x.ptr = __builtin_alloca(sz(num, type) + 1);    \
+    x.ptr = al(sz(num, type) + 1);                  \
     x.max = (type*) x.ptr + (num);                  \
     x.str = x.end = (type*) x.ptr + ((num) >> 1)
 // ______________________________________________________________________________________________
-#define mv0(x)                                      \
-    __builtin_memcpy(x.ptr, x.str, x.end - x.str);  \
-    x.end -= x.str - x.ptr;                         \
+#define mv0(x)                          \
+    mcp(x.ptr, x.str, x.end - x.str);   \
+    x.end -= x.str - x.ptr;             \
     x.str = x.ptr
 #define mvf(x, type)                                                        \
     void *k = (type*) x.str + (((type*) x.max - (type*) x.end + 1) >> 1);   \
-    __builtin_memmove(k, x.str, x.end - x.str);                             \
+    mmv(k, x.str, x.end - x.str);                                           \
     x.end += k - x.str;                                                     \
     x.str = k
 #define mvb(x, type)                                                    \
     void *k = (type*) x.ptr + (((type*) x.str - (type*) x.ptr) >> 1);   \
-    __builtin_memcpy(k, x.str, x.end - x.str);                          \
+    mcp(k, x.str, x.end - x.str);                                       \
     x.end += k - x.str;                                                 \
     x.str = k
 
