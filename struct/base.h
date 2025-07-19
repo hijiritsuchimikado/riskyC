6a90e3d1d90d42099b529fcc6ba376d0f216fd9c
Lemegesor (聖土御門) <lemegesor@zohomail.com>
2025-07-20 01:57:45 +0700

    Update base.h

--- struct/base.h
+++ struct/base.h
@@ -21,22 +21,22 @@ typedef struct {unsigned long *data;} ul1;
 #define dc(x) sml(x.str, x.end)
 
 // start and end from 0
-#define s0i(x)  \
-    pd2 x;      \
+#define s0i(x, type, num)   \
+    pd2 x;                  \
     x.end = x.str = aln(num, type)
 // WARNING: You shouldn't change the value of num here: ++, +=, ...
 // _________________________________________________________________
 // start and end from middle
-#define smi(x)  \
-    pd2 x;      \
+#define smi(x, type, num)   \
+    pd2 x;                  \
     x.end = x.str = (type*) aln(num, type) + ((num) >> 1)
 // start, end and pointer from 0, max at last
-#define s04(x)                      \
+#define s04(x, type, num)           \
     pd4 x;                          \
     x.ptr = al(sz(num, type) + 1);  \
     x.max = (type*) (x.str = x.end = x.ptr) + (num)
 // start and end from middle, pointer from 0 and max at last
-#define sm4(x)                      \
+#define sm4(x, type, num)           \
     pd4 x;                          \
     x.ptr = al(sz(num, type) + 1);  \
     x.max = (type*) x.ptr + (num);  \
@@ -48,24 +48,24 @@ typedef struct {unsigned long *data;} ul1;
     x.end -= x.str - x.ptr;             \
     x.str = x.ptr
 // move front/back to be able to push front/back
-#define mvf(x)                                                              \
+#define mvf(x, type)                                                        \
     void *k = (type*) x.str + (((type*) x.max - (type*) x.end + 1) >> 1);   \
     mmv(k, x.str, x.end - x.str);                                           \
     x.end += k - x.str;                                                     \
     x.str = k
-#define mvb(x)                                                          \
+#define mvb(x, type)                                                    \
     void *k = (type*) x.ptr + (((type*) x.str - (type*) x.ptr) >> 1);   \
     mcp(k, x.str, x.end - x.str);                                       \
     x.end += k - x.str;                                                 \
     x.str = k
 
 // pop front, pop back, push front and push back
-#define pof(x) x.str -= dc(x) * sizeof(type)
-#define pob(x) x.end += dc(x) * sizeof(type)
-#define puf(x)              \
+#define pof(x, type) x.str -= dc(x) * sizeof(type)
+#define pob(x, type) x.end += dc(x) * sizeof(type)
+#define puf(x, type, y)     \
     x.str -= sizeof(type);  \
     *(type*) x.str = (y)
-#define pub(x)              \
+#define pub(x, type, y)     \
     *(type*) x.end = (y);   \
     x.end += sizeof(type)
 
