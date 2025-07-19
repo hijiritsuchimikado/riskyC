7348af594b11022c7238f2fbfd4d47a811b63fff
Lemegesor (聖土御門) <lemegesor@zohomail.com>
2025-07-20 05:34:31 +0700

    Update list.h

--- struct/list.h
+++ struct/list.h
@@ -6,34 +6,58 @@ typedef struct list {
     struct list *next;
 } list;
 
-#define list_new(node1, node2, type, val)           \
-    list *new = __builtin_alloca(sizeof(list));     \
-    new->value = __builtin_alloca(sizeof(type));    \
-    *(type*) new->value = val;                      \
-    new->next = node2;                              \
-    node1 = new
+#define list_val(type, val)     \
+    new->value = al1(type);     \
+    *(type*) new->value = val
+#define list_merge(n1, n2, n3)  \
+    new->next = n1;             \
+    n2 = n3
 #define list_pos(x, pos)            \
     list *tmp = x;                  \
     for (long i = 0; i < pos; ++i)  \
         tmp = tmp->next
+#define list_new(y) \
+    list *new = y;  \
+    for (; new->next; new = new->next)
 
-#define list_init(x) x = NULL;
-#define list_push_front(x, type, val) {list_new(x, x, type, val);}
+#define list_init(x) list *x = NULL;
+// merge/push 1 element to the front of x
+#define list_merge_front(x, y) {    \
+    y->next = x;                    \
+    x = y;                          \
+}
+#define list_push_front(x, type, val) { \
+    list *new = al1(list);              \
+    list_val(type, val);                \
+    list_merge_front(x, new);           \
+}
+// merge a list or push n elements (from array or not) to the front of x
+#define list_merge_list_front(x, y) {   \
+    list_new(y);                        \
+    list_merge(x, x, y);                \
+}
+#define list_push_front_arr(x, type, n, arr) {  \
+    list *new = al1(list);                      \
+    list_val(type, arr[0]);                     \
+    list *tmp = new;                            \
+    for (long i = 1; i < n; ++i) {              \
+        new->next = al1(list);                  \
+        new = new->next;                        \
+        list_val(type, arr[i]);                 \
+    }                                           \
+    list_merge(x, x, tmp);                      \
+}
+#define list_push_front_args(x, type, n, ...) { \
+    type arr[] = {__VA_ARGS__};                 \
+    list_push_front_arr(x, type, n, arr);       \
+}
 // push right after condition == true
-// _________________________________________________________________________________
-#define list_push_ptr(x, type, val) {list_new(x->next, x->next, type, val);}
-#define list_push_pos(x, type, val, pos) {  \
-    list_pos(x, pos);                       \
-    list_push_ptr(tmp, type, val);          \
-}
-// _________________________________________________________________________________
-#define list_pop_front(x) x = x->next
-// delete nodes from after x to y
-#define list_pop_ptr(x, y) x->next = y->next
-// delete a node from after pos
-#define list_pop_pos(x, pos) {      \
-    list_pos(x, pos);               \
-    list_pop_ptr(tmp, tmp->next);   \
-}
+// _____________________________________________________________________________________________
+#define list_merge_ptr(x, y) list_merge_front(x->next, y)
+#define list_push_ptr(x, type, val) list_push_front(x->next, type, val)
+#define list_merge_list_ptr(x, y) list_merge_list_front(x->next, y)
+#define list_push_ptr_arr(x, type, n, arr) list_push_front_arr(x->next, type, n, arr)
+#define list_push_ptr_args(x, type, n, ...) list_push_front_args(x->next, type, n, __VA_ARGS__)
+// _____________________________________________________________________________________________
 
 #endif
