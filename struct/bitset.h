c8864909437f488f549dc99a5a2be10bb42cfcc5
Lemegesor (聖土御門) <lemegesor@zohomail.com>
2025-07-19 22:41:46 +0700

    Update bitset.h

--- struct/bitset.h
+++ struct/bitset.h
@@ -6,7 +6,7 @@
 #define bitset ul1
 
 #define bsp(p) ((p) >> log_lbs)
-#define bsv(x, p) x.data[bsp(p)]
+#define bsv(p) x.data[bsp(p)]
 #define bp(p) (1UL << ((p) & lbsm1))
 #define bss(m) (bsp(m) + 1) << log_lsz
 
@@ -15,9 +15,9 @@
     x.data = al(bss(max));  \
     ms(x.data, 0, bss(max))
 // bitset_read != 0 <=> bit == 1
-#define bitset_read(x, pos) (bsv(x, pos) & bp(pos))
-#define bitset_write_1(x, pos) bsv(x, pos) |= bp(pos)
-#define bitset_write_0(x, pos) bsv(x, pos) &= ~bp(pos)
+#define bitset_read(x, pos) (bsv(pos) & bp(pos))
+#define bitset_write_1(x, pos) bsv(pos) |= bp(pos)
+#define bitset_write_0(x, pos) bsv(pos) &= ~bp(pos)
 
 #endif
 
