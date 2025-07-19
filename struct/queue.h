fbcd8ddbb9df11b841f8157d13ae09ab7f894254
Lemegesor (聖土御門) <lemegesor@zohomail.com>
2025-07-20 02:07:30 +0700

    Update queue.h

--- struct/queue.h
+++ struct/queue.h
@@ -5,8 +5,8 @@
 
 #define queue pd2
 
-#define queue_init(x, type, num) s0i(x)
-#define queue_pop(x, type) pof(x)
-#define queue_push(x, type, y) pub(x)
+#define queue_init(x, type, num) s0i(x, type, num)
+#define queue_pop(x, type) pof(x, type)
+#define queue_push(x, type, y) pub(x, type, y)
 
 #endif
