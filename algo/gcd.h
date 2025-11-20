d9d2ef5595c0891b02218098f35b474e94de99e2
Lemegesor (聖土御門) <lemegesor@zohomail.com>
2025-11-20 12:22:09 +0000

    Update gcd.h

--- algo/gcd.h
+++ algo/gcd.h
@@ -1,15 +1,23 @@
 #ifndef _GCD_H
 #   define _GCD_H
 
-#define gcd(type, a, b) ({  \
-    type _a = a, _b = b;    \
-    for (; _b;) {           \
-        _a %= _b;           \
-        if (!_a) break;     \
-        _b %= _a;           \
-    }                       \
-    _a + _b;                \
+#define gcd(a, b) ({        \
+    typeof(0u | (a) | (b))  \
+    u = (a), v = (b), res;  \
+    if (!u) res = v;        \
+    else {                  \
+        res = ctzg(u | v);  \
+        u >>= res;          \
+        while (v) {         \
+            v >>= ctzg(v);  \
+            if (u > v)      \
+                swap(u, v); \
+            v -= u;         \
+        } res = u << res;   \
+    } res;                  \
 })
-#define lcm(type, a, b) ((type) (a) * (b) / gcd(type, a, b))
+
+
+#define lcm(a, b) ((typeof(0u | (a) | (b))) (a) * (b) / gcd(a, b))
 
 #endif
