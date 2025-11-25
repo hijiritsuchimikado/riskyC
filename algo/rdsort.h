8665d4e98ca358c7af3de3f3bfc38dad3ce36520
Lemegesor (聖土御門) <lemegesor@zohomail.com>
2025-11-25 23:17:54 +0000

    Update rdsort.h

--- algo/rdsort.h
+++ algo/rdsort.h
@@ -3,24 +3,49 @@
 
 #include "../core/base.h"
 
-// not for arrays which can't be swapped via pointers
-#define rdsort(a, n)                                    \
-{                                                       \
-    typedef type(n) T;                                  \
-    T cnt[NUM_16];                                      \
-    type(a) tmp = alc(n * sizeof(a[0]));                \
-    int m = sizeof(type(a[0])) << 3;                    \
-    for (int i = 0; i < m; i += 16)                     \
-    {                                                   \
-        ms(cnt, 0, sizeof(cnt));                        \
-        for (T j = 0; j < n; ++j)                       \
-            ++cnt[(a[j] >> i) & 0xFFFF];                \
-        for (int j = 1; j < NUM_16; ++j)                \
-            cnt[j] += cnt[j - 1];                       \
-        for (T j = n - 1; j >= 0; --j)                  \
-            tmp[--cnt[(a[j] >> i) & 0xFFFF]] = a[j];    \
-        swap(a, tmp);                                   \
-    }                                                   \
+#define rdloop(a, tmp)                              \
+    ms(cnt, 0, sizeof(cnt));                        \
+    for (T j = 0; j < n; ++j)                       \
+        ++cnt[(a[j] >> i) & 0xFFFF];                \
+    for (int j = 1; j < NUM_16; ++j)                \
+        cnt[j] += cnt[j - 1];                       \
+    for (T j = n - 1; j >= 0; --j)                  \
+        tmp[--cnt[(a[j] >> i) & 0xFFFF]] = a[j];    \
+// for pointer arrays only
+#define rdsort_ptr(a, n)                    \
+{                                           \
+    typedef type(n) T;                      \
+    T cnt[NUM_16];                          \
+    int m = sizeof(a[0]) << 3;              \
+    type(a) tmp = alc(n * sizeof(a[0]));    \
+    for (int i = 0; i < m; i += 16)         \
+    {                                       \
+        rdloop(a, tmp);                     \
+        swap(a, tmp);                       \
+    }                                       \
 }
+// for arrays in general, but slower
+#define rdsort_arr(a, n)                    \
+{                                           \
+    typedef type(n) T;                      \
+    T cnt[NUM_16];                          \
+    int m = sizeof(a[0]) << 3;              \
+    type(a[0]) tmp[n];                      \
+    int i = 0;                              \
+    while (1)                               \
+    {                                       \
+        rdloop(a, tmp);                     \
+        i += 16;                            \
+        if (i >= m) {                       \
+            mcp(a, tmp, n * sizeof(a[0]));  \
+            break;                          \
+        }                                   \
+        rdloop(tmp, a);                     \
+        i += 16;                            \
+        if (i >= m) break;                  \
+    }                                       \
+}
+// type = ptr or arr
+#define rdsort(type, a, n) rdsort_##type(a, n)
 
 #endif
