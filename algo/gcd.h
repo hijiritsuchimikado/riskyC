bde52e26b755809c183f8ffea46a70605729d63c
Lemegesor (聖土御門) <lemegesor@zohomail.com>
2025-12-16 18:47:13 +0000

    Update gcd.h

--- algo/gcd.h
+++ algo/gcd.h
@@ -12,7 +12,8 @@
 #define gcd(a, b)                       \
 ({                                      \
     typedef type(0u | (a) | (b)) T;     \
-    GCDATTR inline T gcdf(T u, T v) {   \
+    GCDATTR inline T gcdf(T u, T v)     \
+    {                                   \
         if (u < v) swap(u, v);          \
         if (!v) return u;               \
         u %= v;                         \
@@ -21,9 +22,11 @@
         sh = min(zu, zv);               \
         u >>= zu;                       \
         v >>= zv;                       \
-        do {                            \
+        do                              \
+        {                               \
             T u_v = u - v;              \
-            if (u > v) {                \
+            if (u > v)                  \
+            {                           \
                 u = v;                  \
                 v = u_v;                \
             } else v = -u_v;            \
